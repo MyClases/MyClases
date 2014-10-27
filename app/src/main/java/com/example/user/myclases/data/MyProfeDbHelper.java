367016478e683ace3d329eef01a1f5d9a4765659
diegosfeir <diegosfeir@hotmail.com>
2014-10-27 19:45:32 -0300

    actualización del Helper (crear base de datos)
    todas las restricciones de las tablas, con las llaves y la información de cada tabla.

--- app/src/main/java/com/example/user/myclases/data/MyProfeDbHelper.java
+++ app/src/main/java/com/example/user/myclases/data/MyProfeDbHelper.java
@@ -28,17 +28,68 @@ public class MyProfeDbHelper extends SQLiteOpenHelper {
 
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
         final String createMyProfeSql = String.format("CREATE TABLE %s (" +
                         "%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         "%s TEXT NOT NULL, " +
                         "%s TEXT NOT NULL, " +
-                        "UNIQUE (%s ) ON CONFLICT IGNORE);",
+                        "UNIQUE (%s ) ON CONFLICT IGNORE," +
+                        "%s TEXT NOT NULL," +
+                        "%s TEXT NOT NULL)",
                 MyProfeEntry.TABLE_NAME, MyProfeEntry._ID,
-                MyProfeEntry.COLUMN_NAME, MyProfeEntry.COLUMN_PHONE,
-                MyProfeEntry.COLUMN_PHONE);
+                MyProfeEntry.COLUMN_LOGIN, MyProfeEntry.COLUMN_CLAVE,
+                MyProfeEntry.COLUMN_CLAVE, MyProfeEntry.COLUMN_COMUNA,
+                MyProfeEntry.COLUMN_INFOPERFIL);
 
-        db.execSQL(createMyProfeSql);
+        sqLiteDatabase.execSQL(createMyProfeSql);
+
+        final String createMyProfeSql2 = String.format("CREATE TABLE %s (" +
+                        "%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "%s TEXT NOT NULL, " +
+                        "%s TEXT NOT NULL)",
+                MyProfeEntry2.TABLE_NAME2, MyProfeEntry2._ID,
+                MyProfeEntry2.COLUMN_ASIGNATURA, MyProfeEntry2.COLUMN_CURSO);
+
+        sqLiteDatabase.execSQL(createMyProfeSql2);
+
+        final String createMyProfeSql3 = String.format("CREATE TABLE %s (" +
+                        "%s TEXT NOT NULL, " +
+                        "%s TEXT NOT NULL" +
+                        "%s INTEGER NOT NULL" +
+                        "%s INTEGER NOT NULL" +
+                        "FOREIGN KEY ( %s ) REFERENCES %s (%s)" +
+                        "FOREIGN KEY ( %s ) REFERENCES %s (%s) )",
+                MyProfeEntry3.TABLE_NAME3,
+                MyProfeEntry3.COLUMN_PRECIO,
+                MyProfeEntry3.COLUMN_GRADOACADEMICO,
+                MyProfeEntry3.COLUMN_USUARIOSKEY,
+                MyProfeEntry3.COLUMN_ASIGNATURAKEY,
+                MyProfeEntry3.COLUMN_USUARIOSKEY,
+                MyProfeEntry.TABLE_NAME,
+                MyProfeEntry._ID,
+                MyProfeEntry3.COLUMN_USUARIOSKEY,
+                MyProfeEntry2.TABLE_NAME2,
+                MyProfeEntry2._ID);
+
+        sqLiteDatabase.execSQL(createMyProfeSql3);
+
+        final String createMyProfeSql4 = String.format("CREATE TABLE %s (" +
+                        "%s TEXT NOT NULL, " +
+                        "%s TEXT NOT NULL" +
+                        "FOREIGN KEY ( %s ) REFERENCES %s (%s)" +
+                        "FOREIGN KEY ( %s ) REFERENCES %s (%s) )",
+                MyProfeEntry4.TABLE_NAME4,
+                MyProfeEntry4.COLUMN_USUARIOSKEY,
+                MyProfeEntry4.COLUMN_ASIGNATURAKEY,
+                MyProfeEntry4.COLUMN_USUARIOSKEY,
+                MyProfeEntry.TABLE_NAME,
+                MyProfeEntry._ID,
+                MyProfeEntry4.COLUMN_USUARIOSKEY,
+                MyProfeEntry2.TABLE_NAME2,
+                MyProfeEntry2._ID);
+
+        sqLiteDatabase.execSQL(createMyProfeSql4);
 
     }
     public boolean insertar( String nombre, String telefono) {
