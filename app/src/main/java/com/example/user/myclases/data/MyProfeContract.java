7d8431bc23c6f3686a2d14b29b1fd108f94dbf72
diegosfeir <diegosfeir@hotmail.com>
2014-10-27 19:38:57 -0300

    Agregar tablas de base de datos
    nombres de las cosas que irán en el contract

--- app/src/main/java/com/example/user/myclases/data/MyProfeContract.java
+++ app/src/main/java/com/example/user/myclases/data/MyProfeContract.java
@@ -8,11 +8,45 @@ import android.provider.BaseColumns;
 public class MyProfeContract {
     public static final class MyProfeEntry implements BaseColumns {
 
-        public static final String TABLE_NAME="MyProfe";
+        public static final String TABLE_NAME="Usuarios";
 
-        public static final String COLUMN_NAME="nombre";
+        public static final String COLUMN_LOGIN="Login";
 
-        public static final String COLUMN_PHONE="telefono";
+        public static final String COLUMN_CLAVE="Clave";
+
+        public static final String COLUMN_COMUNA="Comuna";
+
+        public static final String COLUMN_INFOPERFIL="InfoPerfil";
     }
 
+    public static final class MyProfeEntry2 implements BaseColumns {
+
+        public static final String TABLE_NAME2="Asignatura";
+
+        public static final String COLUMN_ASIGNATURA="Asignatura";
+
+        public static final String COLUMN_CURSO="Curso";
+    }
+
+    public static final class MyProfeEntry3 implements BaseColumns {
+
+        public static final String TABLE_NAME3 = "Profesores";
+
+        public static final String COLUMN_PRECIO = "Precio";
+
+        public static final String COLUMN_GRADOACADEMICO = "GradoAcademico";
+
+        public static final String COLUMN_USUARIOSKEY = "IDUsuario";
+
+        public static final String COLUMN_ASIGNATURAKEY = "IDAsignatura";
+    }
+
+    public static final class MyProfeEntry4 implements BaseColumns {
+
+        public static final String TABLE_NAME4="Alumnos";
+
+        public static final String COLUMN_USUARIOSKEY = "IDUsuario";
+
+        public static final String COLUMN_ASIGNATURAKEY = "IDAsignatura";
+    }
 }
